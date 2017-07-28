cb0d0ec8df8f641f6d15e47df79d431922e30905
Scott Sanderson <ssanderson@quantopian.com>
2017-07-28 13:15:59 -0400

    REL: Release 0.5.1.
    - Adds a new `fetch` command to the `pgcontents` CLI for fetching a
      single notebook.
    - Adds utilities for offline processing of notebooks from the pgcontents
      database.
    - Adds utilities for encrypting and decrypting notebooks for many users.
    - Pegs `notebook` dependency to < 5.0.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def main():
 
     setup(
         name='pgcontents',
-        version='0.5.0',
+        version='0.5.1',
         description="A Postgres-backed ContentsManager for IPython/Jupyter.",
         long_description=long_description,
         author="Scott Sanderson",
