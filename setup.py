40c8ab7959aa202bd4e62a8efbeb44acac87d9ec
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-06-01 17:27:33 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ def main():
             'test': test_reqs,
             'ipy3': ['ipython[test,notebook]<4.0'],
             'ipy4': ['ipython<6.0', 'notebook[test]>=4.0,<5.0'],
+            'ipy6': ['ipython>6.0', 'notebook[test]>=5.0']
         },
         scripts=[
             'bin/pgcontents',
