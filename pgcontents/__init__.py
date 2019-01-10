3e57b2f7a9a212fbf6572858b67b5ead7ae7be6d
Scott Sanderson <ssanderson@quantopian.com>
2019-01-10 16:48:19 -0500

    MAINT: Don't require IPython as a dependency.
    It's not needed anymore.

--- pgcontents/__init__.py
+++ pgcontents/__init__.py
@@ -1,16 +1,3 @@
-# Do this first so that we bail early with a useful message if the user didn't
-# specify [ipy3] or [ipy4].
-try:
-    import IPython  # noqa
-except ImportError:
-    raise ImportError(
-        "No IPython installation found.\n"
-        "To install pgcontents with the latest Jupyter Notebook"
-        " run 'pip install pgcontents[ipy4]b'.\n"
-        "To install with the legacy IPython Notebook"
-        " run 'pip install pgcontents[ipy3]'.\n"
-    )
-
 from .checkpoints import PostgresCheckpoints
 from .pgmanager import PostgresContentsManager
 
