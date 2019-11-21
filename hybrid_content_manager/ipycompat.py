0277feba784510e157e96ce809700eefa57b0d3a
Mitchell Sayer <mitchell4408@gmail.com>
2019-11-21 14:28:21 -0800

    Finished setup.py changes

--- hybrid_content_manager/ipycompat.py
+++ hybrid_content_manager/ipycompat.py
@@ -1,8 +1,5 @@
 """Utilities for managing compat between notebook versions."""
 
-#  if notebook.version_info[0] >= 6:  # noqa
-#  raise ImportError("Jupyter Notebook versions 6 and up are not supported.")
-
 from notebook.services.contents.filemanager import FileContentsManager
 from notebook.services.contents.manager import ContentsManager
 from notebook.services.contents.tests.test_manager import TestContentsManager
